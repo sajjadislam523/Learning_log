9a932d562ed7f6472c2b101c7980de2a7170501d
sajjadislam523 <sajjad.islam523@gmail.com>
2022-09-12 00:53:35 +0600

    second commit

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
-    # Home page
+    # Home page for the website.
     path('', views.index, name = 'index'),
 
     # Page that shows all topics
